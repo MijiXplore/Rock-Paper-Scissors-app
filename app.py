39abb74f4b1c50c1ae5df5774597426b2bc9ac60
MijiXplore <shittuolamijiu@gmail.com>
2023-08-13 15:19:26 -0600

    added option to play with friend

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 
 computer_wins = 0
 player_wins = 0
+friend_wins = 0
+f_choice = "rock"
 
 @app.route('/')
 def index():
@@ -12,32 +14,50 @@ def index():
 
 @app.route('/play', methods=['POST'])
 def play():
+    opponent = request.json['opponent']
     options = ['rock', 'paper', 'scissors']
     p_choice = request.json['choice']
     c_choice = random.choice(options)
 
     global computer_wins
     global player_wins
+    global friend_wins
+    global f_choice
+    
+    if opponent == 'Computer':
+        # Logic for playing against the computer
+        # ... (existing logic)
 
-    if p_choice == c_choice:
-        result = 'draw'
-    elif (
-        (p_choice == 'rock' and c_choice == 'scissors') or
-        (p_choice == 'paper' and c_choice == 'rock') or
-        (p_choice == 'scissors' and c_choice == 'paper')
-    ):
-        result = 'win'
-        player_wins += 1  # Increment player wins
-    else:
-        result = 'lose'
-        computer_wins += 1  # Increment computer wins
-
-    return jsonify({
-        'c_choice': c_choice,
-        'result': result,
-        'computer_wins': computer_wins,
-        'player_wins': player_wins
-    })
+        return jsonify({
+            'c_choice': c_choice,
+            'result': result,
+            'computer_wins': computer_wins,
+            'player_wins': player_wins
+        })
+    elif opponent == 'Friend':
+        # Logic for playing against a friend
+        # ... (implement logic for two players)
+        
+        # Compare the choices of the two players
+        if p_choice == f_choice:
+            result = 'draw'
+        elif (
+            (p_choice == 'rock' and f_choice == 'scissors') or
+            (p_choice == 'paper' and f_choice == 'rock') or
+            (p_choice == 'scissors' and f_choice == 'paper')
+        ):
+            result = 'win'
+            player_wins += 1  # Increment player wins
+        else:
+            result = 'lose'
+            friend_wins += 1  # Increment Friend wins
+            
+        return jsonify({
+            'friend_choice': f_choice,
+            'result': result,
+            'friend_wins': friend_wins,
+            'player_wins': player_wins
+        })
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,30 +1,56 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
   const choices = document.querySelectorAll(".choice");
   const resultDiv = document.getElementById("result");
   const computerWinsDiv = document.getElementById("computer-wins");
   const playerWinsDiv = document.getElementById("player-wins");
+  const opponentRadioButtons = document.querySelectorAll('input[name="opponent"]');
 
   choices.forEach((choice) => {
       choice.addEventListener("click", async () => {
+          const selectedOpponent = document.querySelector('input[name="opponent"]:checked');
           const playerChoice = choice.id;
 
-          const response = await fetch("/play", {
-              method: "POST",
-              headers: {
-                  "Content-Type": "application/json",
-              },
-              body: JSON.stringify({ choice: playerChoice }),
-          });
+          if (selectedOpponent) {
+              const opponentValue = selectedOpponent.value;
 
-          const data = await response.json();
-          const computerChoice = data.c_choice;
-          const result = data.result;
-          const computerWins = data.computer_wins;
-          const playerWins = data.player_wins;
+              const response = await fetch("/play", {
+                  method: "POST",
+                  headers: {
+                      "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                      opponent: opponentValue,
+                      choice: playerChoice,
+                  }),
+              });
 
-          resultDiv.innerHTML = `You chose ${playerChoice}. Computer chose ${computerChoice}. You ${result}!`;
-          computerWinsDiv.innerHTML = `Computer wins: ${computerWins}`;
-          playerWinsDiv.innerHTML = `You win: ${playerWins}`;
+              const data = await response.json();
+
+              if (opponentValue === 'Computer') {
+                  // Logic for displaying result when playing against the computer
+                  const computerChoice = data.c_choice;
+                  const result = data.result;
+                  const computerWins = data.computer_wins;
+                  const playerWins = data.player_wins;
+
+                  resultDiv.innerHTML = `You chose ${playerChoice}. Computer chose ${computerChoice}. You ${result}!`;
+                  computerWinsDiv.innerHTML = `Computer wins: ${computerWins}`;
+                  playerWinsDiv.innerHTML = `You win: ${playerWins}`;
+              } else if (opponentValue === 'Friend') {
+                  // Logic for displaying result when playing against a friend
+                  const friendChoice = data.friend_choice;
+                  const result = data.result;
+                  const friendWins = data.friend_wins;
+                  const playerWins = data.player_wins;
+
+                  resultDiv.innerHTML = `You chose ${playerChoice}. Friend chose ${friendChoice}. You ${result}!`;
+                  computerWinsDiv.innerHTML = `Friend wins: ${friendWins}`;
+                  playerWinsDiv.innerHTML = `You win: ${playerWins}`;
+              }
+          } else {
+              // Handle the case when no opponent is selected
+              console.log("Please select an opponent.");
+          }
       });
   });
 });
